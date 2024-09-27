685f938938bf0424c77f775b8a1e630993c3ada0
LudwingValecillos <ludwingvaldev@gmail.com>
2024-09-27 16:40:42 -0300

    actualizacion vista events

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Events from "./pages/Events";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,6 +18,7 @@ function App() {
       <Route path="/" element={<MainLayout />}>
       <Route index element={<Navigate to="Home" replace />} />
       <Route path="home" element={<Home />} />
+      <Route path="events" element={<Events />} />
       </Route>
       <Route path="/register" element={<Register />} />
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
     <nav aria-label="Global" className="hidden md:block">
               <ul className="flex flex-wrap justify-center font-bold gap-6 md:gap-8 lg:gap-12">
+                <li>
+                  <Link to="/home"
+                    className="transition hover:text-gray-500/75"
+                    href="#"
+                  >
+                    {" "}
+                    Home{" "}
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to="/events"
+                    className="transition hover:text-gray-500/75"
+                    href="#"
+                  >
+                    {" "}
+                    Events{" "}
+                  </Link>
+                </li>
+
                 <li>
                   <a
                     className="transition hover:text-gray-500/75"
@@ -11,26 +32,6 @@ function Nav() {
                   >
                     {" "}
                     About{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    Careers{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    History{" "}
                   </a>
                 </li>
 
