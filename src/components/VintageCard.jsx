58d1f72157e3985df47843788823c0b4248b7701
LudwingValecillos <ludwingvaldev@gmail.com>
2024-09-30 16:29:15 -0300

    a

--- src/components/VintageCard.jsx
+++ src/components/VintageCard.jsx
@@ -17,10 +17,10 @@ const VintageCard = ({ title, imageUrl, buttonText, backgroundColor, fontFamily 
 
       {/* Título de la izquierda */}
       <motion.h2
-        className="absolute bottom-12 left-0 w-full text-left text-xl font-serif text-white z-10 px-4" // Asegura que ocupe el ancho total
+        className="absolute bottom-12 left-0 w-full text-left text-2xl font-serif text-white z-10 px-4" // Asegura que ocupe el ancho total
         initial={{ x: '-100%' }} // Comienza fuera de la pantalla a la izquierda
         animate={{ x: ['-100%', '100%'] }} // Se mueve de izquierda a derecha
-        transition={{ duration: 15, ease: 'linear', repeat: Infinity }} // Movimiento continuo
+        transition={{ duration: 5, ease: 'linear', repeat: Infinity }} // Movimiento continuo
         style={{ fontFamily }}
       >
         {title}
@@ -28,10 +28,10 @@ const VintageCard = ({ title, imageUrl, buttonText, backgroundColor, fontFamily 
 
       {/* Título de la derecha */}
       <motion.h2
-        className="absolute bottom-4 right-0 w-full text-right text-xl font-serif text-white z-10 px-4" // Asegura que ocupe el ancho total
+        className="absolute bottom-4 right-0 w-full text-right text-2xl font-serif text-white z-10 px-4" // Asegura que ocupe el ancho total
         initial={{ x: '100%' }} // Comienza fuera de la pantalla a la derecha
         animate={{ x: ['100%', '-100%'] }} // Se mueve de derecha a izquierda
-        transition={{ duration: 15, ease: 'linear', repeat: Infinity }} // Movimiento continuo
+        transition={{ duration: 5, ease: 'linear', repeat: Infinity }} // Movimiento continuo
         style={{ fontFamily }}
       >
         {title}
