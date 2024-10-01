2e037b5a2b9aaec3349a40d6e8f7c027902e5c85
psilocyamp <ampienmonte@gmail.com>
2024-10-01 16:12:28 -0300

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -82,7 +82,7 @@ const Login = () => {
 
     return (
         <div className="relative overflow-hidden h-screen">
-            <div className="absolute inset-0 bg-gradient-to-r from-blue-500 to-green-400 opacity-70"></div>
+            <div className="absolute inset-0 "></div>
             <div className="absolute inset-0 bg-no-repeat bg-center bg-cover" style={{ backgroundImage: "url('https://source.unsplash.com/random/1920x1080?color')", opacity: 0.2 }}></div>
             <div className="flex items-center justify-center h-full relative z-10">
                 <div className="bg-[#f5f5f5c9] rounded-lg  shadow-lg p-8 w-full max-w-xl text-center transition-transform transform hover:scale-105 hover:shadow-2xl hover:bg-[#f5f5f5fa]">
