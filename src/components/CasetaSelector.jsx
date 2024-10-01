f6625106b934f3c90c635f99ef6bb35c03f6ce2e
psilocyamp <ampienmonte@gmail.com>
2024-10-01 01:55:13 -0300

    casetas changes

--- src/components/CasetaSelector.jsx
+++ src/components/CasetaSelector.jsx
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
 
 const CasetaSelector = () => {
-    const smallCasetaWidth = 30;  // Casetas pequeñas
+    const smallCasetaWidth = 30;
     const smallCasetaHeight = 30;
-    const largeCasetaWidth = smallCasetaWidth * 2;  // Casetas grandes
+    const largeCasetaWidth = smallCasetaWidth * 2;
     const largeCasetaHeight = smallCasetaHeight;
-    
-    const pasilloWidth = 15; // Espacio entre pasillos
 
-    // Estado para manejar el hover y la selección
+    const pasilloWidth = 15;
     const [hoveredCaseta, setHoveredCaseta] = useState(null);
     const [selectedCasetas, setSelectedCasetas] = useState([]);
     const [showModal, setShowModal] = useState(false);
 
-    // Manejar hover
     const handleMouseEnter = (casetaNumber) => {
         setHoveredCaseta(casetaNumber);
     };
@@ -22,17 +19,14 @@ const CasetaSelector = () => {
         setHoveredCaseta(null);
     };
 
-    // Manejar selección de casetas
     const handleCasetaClick = (casetaNumber) => {
         if (selectedCasetas.includes(casetaNumber)) {
-            // Si la caseta ya está seleccionada, se deselecciona
             setSelectedCasetas(selectedCasetas.filter(caseta => caseta !== casetaNumber));
         } else if (selectedCasetas.length < 2) {
-            // Si hay menos de 2 casetas seleccionadas, se selecciona una nueva
             setSelectedCasetas([...selectedCasetas, casetaNumber]);
             setShowModal(true);
         } else {
-            alert("You can only select a maximum of 2 casetas.");
+            alert("Solo puedes seleccionar un máximo de 2 casetas.");
         }
     };
 
@@ -48,11 +42,11 @@ const CasetaSelector = () => {
     };
 
     return (
-        <div className="flex justify-between items-center h-screen bg-gradient-to-r from-blue-200 to-blue-500">
-            <svg viewBox="0 0 500 350" className="bg-white border-4 border-gray-500 shadow-xl rounded-lg">
+        <div className="flex justify-between items-center h-screen bg-gradient-to-r from-blue-300 to-blue-500">
+            <svg viewBox="0 0 500 350" className="bg-white border-4 border-gray-400 shadow-lg rounded-lg">
                 {/* Escenario */}
-                <rect x="200" y="20" width="100" height="40" className="fill-red-600 shadow-lg"></rect>
-                <text x="250" y="45" className="text-xs font-bold fill-white" textAnchor="middle">Stage</text>
+                <rect x="200" y="20" width="100" height="40" className="fill-red-600 shadow-md rounded-lg"></rect>
+                <text x="250" y="45" className="text-xs font-bold fill-white" textAnchor="middle">Escenario</text>
 
                 {/* Casetas Grandes */}
                 {[...Array(5)].map((_, index) => (
@@ -62,10 +56,10 @@ const CasetaSelector = () => {
                             y={80 + index * (largeCasetaHeight + pasilloWidth)}
                             width={largeCasetaWidth}
                             height={largeCasetaHeight}
-                            className={`fill-gray-300 stroke-gray-600 stroke-2 cursor-pointer transition duration-200 ease-in-out 
-                                ${hoveredCaseta === index + 1 ? 'stroke-4' : 'stroke-2'}
-                                ${isSelected(index + 1) ? 'fill-blue-400' : 'fill-gray-300'}
-                                shadow-md`}
+                            className={`fill-blue-300 stroke-gray-600 stroke-2 cursor-pointer transition-transform duration-300 ease-in-out 
+                                ${hoveredCaseta === index + 1 ? 'scale-105' : 'scale-100'}
+                                ${isSelected(index + 1) ? 'fill-blue-500' : 'fill-blue-300'}
+                                shadow-lg rounded-lg`}
                             onMouseEnter={() => handleMouseEnter(index + 1)}
                             onMouseLeave={handleMouseLeave}
                             onClick={() => handleCasetaClick(index + 1)}
@@ -74,7 +68,7 @@ const CasetaSelector = () => {
                             <text
                                 x={50}
                                 y={100 + index * (largeCasetaHeight + pasilloWidth)}
-                                className="fill-black text-sm font-semibold"
+                                className="fill-black text-lg font-semibold"
                             >
                                 {index + 1}
                             </text>
@@ -89,10 +83,10 @@ const CasetaSelector = () => {
                             y={80 + index * (largeCasetaHeight + pasilloWidth)}
                             width={largeCasetaWidth}
                             height={largeCasetaHeight}
-                            className={`fill-gray-300 stroke-gray-600 stroke-2 cursor-pointer transition duration-200 ease-in-out 
-                                ${hoveredCaseta === index + 6 ? 'stroke-4' : 'stroke-2'}
-                                ${isSelected(index + 6) ? 'fill-blue-400' : 'fill-gray-300'}
-                                shadow-md`}
+                            className={`fill-blue-300 stroke-gray-600 stroke-2 cursor-pointer transition-transform duration-300 ease-in-out 
+                                ${hoveredCaseta === index + 6 ? 'scale-105' : 'scale-100'}
+                                ${isSelected(index + 6) ? 'fill-blue-500' : 'fill-blue-300'}
+                                shadow-lg rounded-lg`}
                             onMouseEnter={() => handleMouseEnter(index + 6)}
                             onMouseLeave={handleMouseLeave}
                             onClick={() => handleCasetaClick(index + 6)}
@@ -101,7 +95,7 @@ const CasetaSelector = () => {
                             <text
                                 x={470}
                                 y={100 + index * (largeCasetaHeight + pasilloWidth)}
-                                className="fill-black text-sm font-semibold"
+                                className="fill-black text-lg font-semibold"
                             >
                                 {index + 6}
                             </text>
@@ -118,10 +112,10 @@ const CasetaSelector = () => {
                                 y={80 + rowIndex * (smallCasetaHeight + pasilloWidth)}
                                 width={smallCasetaWidth}
                                 height={smallCasetaHeight}
-                                className={`fill-gray-300 stroke-gray-600 stroke-2 cursor-pointer transition duration-200 ease-in-out 
-                                    ${hoveredCaseta === 10 + rowIndex * 5 + colIndex + 1 ? 'stroke-4' : 'stroke-2'}
-                                    ${isSelected(10 + rowIndex * 5 + colIndex + 1) ? 'fill-blue-400' : 'fill-gray-300'}
-                                    shadow-md`}
+                                className={`fill-green-300 stroke-gray-600 stroke-2 cursor-pointer transition-transform duration-300 ease-in-out 
+                                    ${hoveredCaseta === 10 + rowIndex * 5 + colIndex + 1 ? 'scale-105' : 'scale-100'}
+                                    ${isSelected(10 + rowIndex * 5 + colIndex + 1) ? 'fill-green-500' : 'fill-green-300'}
+                                    shadow-lg rounded-lg`}
                                 onMouseEnter={() => handleMouseEnter(10 + rowIndex * 5 + colIndex + 1)}
                                 onMouseLeave={handleMouseLeave}
                                 onClick={() => handleCasetaClick(10 + rowIndex * 5 + colIndex + 1)}
@@ -130,7 +124,7 @@ const CasetaSelector = () => {
                                 <text
                                     x={165 + colIndex * (smallCasetaWidth + pasilloWidth)}
                                     y={100 + rowIndex * (smallCasetaHeight + pasilloWidth)}
-                                    className="fill-black text-sm font-semibold"
+                                    className="fill-black text-lg font-semibold"
                                 >
                                     {10 + rowIndex * 5 + colIndex + 1}
                                 </text>
@@ -140,17 +134,17 @@ const CasetaSelector = () => {
                 ))}
 
                 {/* Entrada y Salida */}
-                <rect x="20" y="300" width="80" height="30" className="fill-green-400 stroke-green-600 stroke-2 shadow-lg"></rect>
-                <text x="60" y="320" className="text-xs font-bold fill-black" textAnchor="middle">Entrance</text>
+                <rect x="20" y="300" width="80" height="30" className="fill-green-500 stroke-green-600 stroke-2 shadow-lg rounded-lg"></rect>
+                <text x="60" y="320" className="text-xs font-bold fill-white" textAnchor="middle">Entrada</text>
 
-                <rect x="400" y="300" width="80" height="30" className="fill-green-400 stroke-green-600 stroke-2 shadow-lg"></rect>
-                <text x="440" y="320" className="text-xs font-bold fill-black" textAnchor="middle">Exit</text>
+                <rect x="400" y="300" width="80" height="30" className="fill-green-500 stroke-green-600 stroke-2 shadow-lg rounded-lg"></rect>
+                <text x="440" y="320" className="text-xs font-bold fill-white" textAnchor="middle">Salida</text>
             </svg>
 
             {/* Modal con casetas seleccionadas */}
             {showModal && (
-                <div className="fixed right-0 top-0 h-full w-64 bg-gray-200 shadow-lg p-4">
-                    <h2 className="text-lg font-semibold mb-4">Selected Casetas</h2>
+                <div className="fixed right-0 top-0 h-full w-64 bg-white shadow-lg p-4 rounded-lg">
+                    <h2 className="text-lg font-semibold mb-4">Casetas Seleccionadas</h2>
                     <ul>
                         {selectedCasetas.map((casetaNumber) => (
                             <li key={casetaNumber} className="mb-2">
@@ -159,10 +153,10 @@ const CasetaSelector = () => {
                         ))}
                     </ul>
                     <button
-                        className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700"
+                        className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-700 transition duration-300"
                         onClick={() => setShowModal(false)}
                     >
-                        Close
+                        Cerrar
                     </button>
                 </div>
             )}
