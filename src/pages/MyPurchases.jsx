5b46146ceb2cf3c554a4bdbd2b0bd6a7fc13b738
carolinabpineiro <carolinabpineiro@gmail.com>
2024-10-03 14:30:57 -0300

    Actualizaion de profile

--- src/pages/MyPurchases.jsx
+++ src/pages/MyPurchases.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'; 
 import { useDispatch, useSelector } from 'react-redux';
-import { loadClient } from '../redux/actions/clientActions'; // Asegúrate de que la ruta sea correcta
+import { loadClient } from '../redux/actions/clientActions'; 
 import CardForm from '../components/CardForm';
 
 export default function PerfilUsuario() {
   const dispatch = useDispatch();
-  const { client, status, error } = useSelector((state) => state.client); // Obtén el estado del cliente
+  const { client, status, error } = useSelector((state) => state.client);
 
   const [activeTab, setActiveTab] = useState("perfil");
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -22,22 +22,33 @@ export default function PerfilUsuario() {
   }, []);
 
   useEffect(() => {
-    // Carga los datos del cliente al montar el componente
     dispatch(loadClient());
   }, [dispatch]);
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4" onMouseMove={handleMouseMove}>
       <div className="w-full max-w-4xl bg-white rounded-lg shadow-xl overflow-hidden border-black border-[1px]">
-        <div className="relative pb-20 bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-          <div className="absolute -bottom-16 left-1/2 transform -translate-x-1/2">
-            <div className="w-32 h-32 rounded-full border-4 border-white flex justify-center items-center text-2xl font-bold bg-gray-300">
-              {client.firstName[0]}{client.lastName[0]}
-            </div>
+      <div className="relative pb-20 bg-gradient-to-r from-[#f2bb13] to-[#f28d35] text-white">
+
+
+        <div className="absolute -bottom-16 left-1/2 transform -translate-x-1/2">
+            <img 
+              src="https://d3n32ilufxuvd1.cloudfront.net/635fde9e3d2caa0029c91035/4190189/Image-0157de3b-2ef9-45c1-ba67-9bad1e1925fb.gif" 
+              alt="Wave Center" 
+              className="w-32 h-32 rounded-full border-4 border-grey-500 object-cover"
+            />
           </div>
           <div className="text-center mt-8">
             <h2 className="text-3xl font-bold">{client.firstName} {client.lastName}</h2>
-            <p className="text-xl text-gray-200">Miembro de Wave Center</p>
+            <p className="text-xl text-gray-200">Member of Wave Center</p>
+          </div>
+
+          {/* Descripción de bienvenida */}
+          <div className="mt-6 text-center">
+            <h3 className="text-2xl font-semibold mb-2">So Glad You're Here! 🌟</h3>
+            <p className="text-lg text-gray-100 max-w-xl mx-auto">
+              Discover a place where culture, creativity, and community come together. At Wave Center, we invite you to enjoy music 🎵, art 🎨, captivating exhibitions, and unique events. Join our vibrant community and experience something new every time you visit. The center is yours!
+            </p>
           </div>
         </div>
 
